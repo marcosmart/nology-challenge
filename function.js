6bd8825325a6d7602fd3c4bba72fe2341b662fab
Marcos Eduardo Martinez Rodriguez <63803603+marcosmart@users.noreply.github.com>
2020-09-03 21:34:44 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,13 @@
-$(document).ready(function () {
-    $('.nav_btn').click(function () {
-      $('.mobile_nav_items').toggleClass('active');
-    });
-  });
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'pink');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
